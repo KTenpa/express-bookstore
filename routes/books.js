7af70de8e900f396ef5875b4585588a70335cea7
Karma Tenpa <ktenpa@mac.mynetworksettings.com>
2025-04-15 19:26:43 -0500

    Add schemas & update put and post routes

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,9 @@ const Book = require("../models/book");
 
 const router = new express.Router();
 
+const { validate } = require("jsonschema");
+const bookSchemaNew = require("../schemas/bookSchemaNew");
+const bookSchemaUpdate = require("../schemas/bookSchemaUpdate");
 
 /** GET / => {books: [book, ...]}  */
 
@@ -17,9 +20,9 @@ router.get("/", async function (req, res, next) {
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get("/:isbn", async function (req, res, next) {
   try {
-    const book = await Book.findOne(req.params.id);
+    const book = await Book.findOne(req.params.isbn);
     return res.json({ book });
   } catch (err) {
     return next(err);
@@ -30,6 +33,13 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const validation = validate(req.body, bookSchemaNew);
+    if (!validation.valid) {
+      return next({
+        status: 400,
+        error: validation.errors.map(e => e.stack)
+      });
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -41,6 +51,19 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    if ("isbn" in req.body) {
+      return next({
+        status: 400,
+        message: "Not allowed"
+      });
+    }
+    const validation = validate(req.body, bookSchemaUpdate);
+    if (!validation.valid) {
+      return next({
+        status: 400,
+        errors: validation.errors.map(e => e.stack)
+      });
+    }
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
